d463d8d4abff1160409ec6c275523bed770d8a0a
Usman Khan <u.khan980212@gmail.com>
2020-12-15 22:26:26 -0500

    Usman 10 26. project initlaized (had to watch youtube video fml)

---
+++ Usman_Khan/Models/Expense.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Usman_Khan.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public double Amount { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
