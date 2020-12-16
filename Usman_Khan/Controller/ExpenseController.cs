16dc26d6196197c7318db103c392027183115404
Usman Khan <u.khan980212@gmail.com>
2020-12-15 23:23:53 -0500

    Final commit, My browser got stuck on add a file, 11:23pm. added controller.

--- Usman_Khan/Controller/ExpenseController.cs
+++ Usman_Khan/Controller/ExpenseController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Usman_Khan.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace Usman_Khan.Controller
 {
@@ -11,5 +14,95 @@ namespace Usman_Khan.Controller
     [ApiController]
     public class ExpenseController : ControllerBase
     {
+
+
+        private ExpenseDatabase _database = new ExpenseDatabase();
+
+
+        [HttpGet]
+        public IEnumerable<Expense> Get()
+        {
+            return _database.Expenses;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var item = await _database.Expenses.FindAsync(id);
+            if (item == null)
+                return NotFound(new { error = "id is not found" });
+            else
+                return Ok(item);
+        }
+
+        /*    [HttpGet("{id}/{year}/{month}")]
+            public async Task<IActionResult> Get(int id, int year, int month)
+            {
+                var item = await _database.Items.FindAsync(id);
+                if (item == null)
+                    return NotFound(new { error = "id is not found" });
+                else
+                    return Ok(item);
+            } */
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Expense expItem)
+        {
+            _database.Expenses.Add(expItem);
+            await _database.SaveChangesAsync();
+            return Ok(expItem);
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTodoItem(int id, Expense expItems)
+        {
+            if (id != expItems.Id)
+            {
+                return BadRequest();
+            }
+
+            var expItem = await _database.Expenses.FindAsync(id);
+            if (expItem == null)
+            {
+                return NotFound();
+            }
+
+            expItem.Description = expItems.Description;
+            expItem.Amount = expItems.Amount;
+            expItem.Date = expItems.Date;
+            expItem.UserId = expItems.UserId;
+
+            try
+            {
+                await _database.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException) when (!TodoItemExists(id))
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _database.Expenses.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _database.Expenses.Remove(todoItem);
+            await _database.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TodoItemExists(int id) => _database.Expenses.Any(e => e.Id == id);
+
     }
 }
